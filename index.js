f9e9a085d96d2a27955653898199e0786d68f816
H0rn0chse <h0rn0chse@gmx.de>
2022-02-25 21:18:31 +0100

    Add dist folder

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "url";    
 import * as core from "@actions/core";
 import * as io from "@actions/io";
 
